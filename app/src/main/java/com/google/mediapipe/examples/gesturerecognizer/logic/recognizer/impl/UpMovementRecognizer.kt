b4834a3047e26b2c06dec7cc5f595c032d349dd2
Architv27 <archit.sfu@gmail.com>
2023-11-26 16:47:30 -0800

    Up movementRecognizer file updated

--- app/src/main/java/com/google/mediapipe/examples/gesturerecognizer/logic/recognizer/impl/UpMovementRecognizer.kt
+++ app/src/main/java/com/google/mediapipe/examples/gesturerecognizer/logic/recognizer/impl/UpMovementRecognizer.kt
@@ -9,13 +9,18 @@ class UpMovementRecognizer : DynamicGestureRecognizer {
     override fun checkHandMovement(gestureList: List<GestureWrapper>, landmarkIndex: Int): Boolean {
         if (gestureList.size < minLength)
             return false
-        val movements = mutableListOf<Float>()
-        for (i in gestureList.size - 1 downTo 1) {
-            val movement = comparePositions(gestureList[i], gestureList[i - 1], landmarkIndex)
-            Log.i("checkHandMovement", "$movement between indexed $i and ${i - 1}")
-            movements.add(movement)
+
+        val movementValues = mutableListOf<Float>()
+        var currentIndex = gestureList.size - 1
+
+        while (currentIndex >= 1) {
+            val currentMovement = comparePositions(gestureList[currentIndex], gestureList[currentIndex - 1], landmarkIndex)
+            Log.i("checkHandMovement", "$currentMovement between indexed $currentIndex and ${currentIndex - 1}")
+            movementValues.add(currentMovement)
+            currentIndex--
         }
-        return movements.all { it > movementThreshold }
+
+        return movementValues.all { it > movementThreshold }
     }
 
     private fun comparePositions(
