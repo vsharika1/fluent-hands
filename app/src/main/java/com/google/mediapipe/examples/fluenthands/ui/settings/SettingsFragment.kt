d40cb6f98bed71f9aaea981f6614658cee85e865
Daniel <dkolocka@sfu.ca>
2023-12-02 14:39:49 -0800

    Logout implemented

--- app/src/main/java/com/google/mediapipe/examples/fluenthands/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/google/mediapipe/examples/fluenthands/ui/settings/SettingsFragment.kt
@@ -9,8 +9,10 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 import com.google.mediapipe.examples.fluenthands.ProfileActivity
 import com.google.mediapipe.examples.fluenthands.R
+import com.google.mediapipe.examples.fluenthands.SignInActivity
 
 class SettingsFragment : Fragment() {
     private lateinit var helloName: TextView
@@ -20,16 +22,16 @@ class SettingsFragment : Fragment() {
     private lateinit var averageScore: TextView
     private lateinit var userProfile: Button
     private lateinit var accountCreated: TextView
+    private lateinit var logOutButton: Button
+
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
         var view = inflater.inflate(R.layout.fragment_settings2, container, false)
 
-//        INSERT FIREBASE CODE TO FETCH DATA
-//        **
-        //**
-//        **
+        firebaseAuth = FirebaseAuth.getInstance()
 
 //        Get xml items for udpating UI
         helloName = view.findViewById(R.id.helloSettings)
@@ -39,6 +41,14 @@ class SettingsFragment : Fragment() {
         averageScore = view.findViewById(R.id.settingsScore)
         userProfile = view.findViewById(R.id.userProfileButton)
         accountCreated = view.findViewById(R.id.dateCreated)
+        logOutButton = view.findViewById(R.id.logoutButton)
+        logOutButton.setOnClickListener {
+            firebaseAuth.signOut()
+            val intent = Intent(requireContext(), SignInActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+
+        }
 
         userProfile.setOnClickListener{
             val activity = requireActivity()
