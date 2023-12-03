06ba410ddde260922fb14c910228157f6e1bd8b5
Janit Kumar <kjanit15@gmail.com>
2023-12-03 01:14:07 -0800

    add viewModel file

--- app/src/main/java/com/google/mediapipe/examples/fluenthands/db/Result.kt
+++ app/src/main/java/com/google/mediapipe/examples/fluenthands/db/Result.kt
@@ -1,11 +1,15 @@
 package com.google.mediapipe.examples.fluenthands.db
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 
 @Entity (tableName = "results_table")
 data class Result (
-    @PrimaryKey(autoGenerate = true) var id: Long = 0L,
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0L,
 
+    @ColumnInfo(name = "quiz")
+    var quiz: String = ""
     )
--- app/src/main/java/com/google/mediapipe/examples/fluenthands/db/ResultViewModel.kt
+++ app/src/main/java/com/google/mediapipe/examples/fluenthands/db/ResultViewModel.kt
@@ -2,7 +2,9 @@ package com.google.mediapipe.examples.fluenthands.db
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
+import java.lang.IllegalArgumentException
 
 
 class ResultViewModel(private var repository: ResultRepository): ViewModel() {
@@ -20,5 +22,14 @@ class ResultViewModel(private var repository: ResultRepository): ViewModel() {
 
     fun deleteResultTable() {
         repository.deleteResultTable()
+    }
+}
+
+
+class ResultViewModelFactory (private val repository: ResultRepository) : ViewModelProvider.Factory {
+    override fun<T: ViewModel> create(modelClass: Class<T>) : T{ 
+        if(modelClass.isAssignableFrom(ResultViewModel::class.java))
+            return ResultViewModel(repository) as T
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
