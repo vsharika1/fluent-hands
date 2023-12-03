6392882a458a9f6831740bd9213ac0a3f495e75d
Architv27 <archit.sfu@gmail.com>
2023-12-02 20:55:04 -0800

    .png files for alphabet added

--- app/src/main/java/com/google/mediapipe/examples/fluenthands/LearningPage.kt
+++ app/src/main/java/com/google/mediapipe/examples/fluenthands/LearningPage.kt
@@ -19,6 +19,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -31,6 +32,15 @@ class LearningPage : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityLearningPageBinding
     private val viewModel: MainViewModel by viewModels()
     private lateinit var backButton: Button
+    private val letterDrawables = arrayOf(
+        R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e,
+        R.drawable.f, R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.j,
+        R.drawable.k, R.drawable.l, R.drawable.m, R.drawable.n, R.drawable.o,
+        R.drawable.p, R.drawable.q, R.drawable.r, R.drawable.s, R.drawable.t,
+        R.drawable.u, R.drawable.v, R.drawable.w, R.drawable.x, R.drawable.y,
+        R.drawable.z
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,12 +53,21 @@ class LearningPage : AppCompatActivity() {
         activityMainBinding.navigation.setOnNavigationItemReselectedListener {
             // ignore the reselection
         }
-
+        setImage(0)
         val backButton = findViewById<ImageButton>(R.id.back_button)
         backButton.setOnClickListener {
             finish()
         }
     }
+    fun setImage(index: Int) {
+        val imageView = findViewById<ImageView>(R.id.camera_overlay)
+        if (index in letterDrawables.indices) {
+            imageView.setImageResource(letterDrawables[index])
+        } else {
+            imageView.setImageResource(R.drawable.fluenthands) // Default or fallback drawable
+        }
+    }
+
 
     override fun onBackPressed() {
         finish()
