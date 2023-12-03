6e5e0d3a36ee602b5b71df643f7fc897d0c3b122
Varun <varuncifer@gmail.com>
2023-12-02 20:55:48 -0800

    pick random word for quiz in MainActivity

--- app/src/main/java/com/google/mediapipe/examples/fluenthands/MainActivity.kt
+++ app/src/main/java/com/google/mediapipe/examples/fluenthands/MainActivity.kt
@@ -32,6 +32,15 @@ class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
     private lateinit var backButton: Button
 
+    // Array containing alphabets from a to z
+    private val easy = ('a'..'z').toList().toCharArray()
+
+    // Array containing a list of 3-letter words without the alphabet 'v'
+    private val medium = arrayOf("cat", "dog", "bat", "hat", "run", "fun", "sun", "car", "bar", "jar", "pen", "hen", "fox", "box", "cup", "rug", "mug", "bug", "big", "dig")
+
+    // Array containing a list of 5-letter words without the alphabet 'v'
+    private val difficult = arrayOf("house", "mouse", "apple", "happy", "cloud", "table", "chair", "beach", "earth", "river", "mount", "smile", "grape", "melon", "snake", "plane", "ocean", "knife", "honey", "music")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -48,10 +57,34 @@ class MainActivity : AppCompatActivity() {
         backButton.setOnClickListener {
             finish()
         }
+
+        val difficulty = Difficulty.EASY // Replace with the actual difficulty level
+        displayRandomWord(difficulty)
+    }
+
+    enum class Difficulty {
+        EASY, MEDIUM, HARD
     }
 
     override fun onBackPressed() {
         finish()
+    }
+
+    private fun getRandomWord(difficulty: Difficulty): String {
+        return when (difficulty) {
+            Difficulty.EASY -> {
+                // Return a random letter from the alphabets array
+                easy.random().toString()
+            }
+            Difficulty.MEDIUM -> {
+                // Return a random 3-letter word without 'v' from the words3LetterWithoutV array
+                medium.random()
+            }
+            Difficulty.HARD -> {
+                // Return a random 5-letter word without 'v' from the words5LetterWithoutV array
+                difficult.random()
+            }
+        }
     }
 
     fun deletebuttonClick(view: View?) {
@@ -69,5 +102,10 @@ class MainActivity : AppCompatActivity() {
         val textLabel = findViewById<TextView>(R.id.textLabel)
         textLabel.text = textLabel.text.toString() + " "
         ContextHolder.currentWord = textLabel.text.toString() + " "
+    }
+
+    private fun displayRandomWord(difficulty: Difficulty) {
+        val randomWord = getRandomWord(difficulty)
+        findViewById<TextView>(R.id.wordTextView).text = randomWord
     }
 }
