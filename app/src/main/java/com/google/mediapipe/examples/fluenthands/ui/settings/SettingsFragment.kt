b0586597b943fc41b85a69d630474e3e4a88f5df
Daniel <dkolocka@sfu.ca>
2023-12-04 16:26:00 -0800

    Profile Photo implemented between profile activity and settings fragment

--- app/src/main/java/com/google/mediapipe/examples/fluenthands/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/google/mediapipe/examples/fluenthands/ui/settings/SettingsFragment.kt
@@ -5,6 +5,7 @@ import android.app.AlertDialog
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -117,6 +118,8 @@ class SettingsFragment : Fragment() {
     }
 
     private fun fetchData() {
+        val sharedPreferences = requireActivity().getSharedPreferences("profilePicture", MODE_PRIVATE)
+        userPicture.setImageURI(Uri.parse(sharedPreferences.getString("imgUri", "")))
 //        Get data from DB then set values
         val user: FirebaseUser? = firebaseAuth.currentUser
         if (user != null) {
